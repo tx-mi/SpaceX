07d91bd3f2bace4f071309ad9b6eea7b4ad2d984
tx-mi <tx-mi@gmail.com>
2022-07-23 04:15:57 +0300

    Create Empty project without storyboards

---
+++ SpaceXApp/SpaceXApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceXApp
+//
+//  Created by Ramazan Abdulaev on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
