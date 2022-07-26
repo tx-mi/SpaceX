128f298ac14e1e0864cc634f1f5f0f041a318880
tx-mi <tx-mi@gmail.com>
2022-07-26 22:07:48 +0300

    ext color, ext view for round corners

--- SpaceXApp/SpaceXApp/Extensions/UIColor+Ext.swift
+++ SpaceXApp/SpaceXApp/Extensions/UIColor+Ext.swift
@@ -5,4 +5,22 @@
 //  Created by Ramazan Abdulaev on 26.07.2022.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let blondGray = UIColor(hex: 0x212121)
+    static let textGray = UIColor(hex: 0x8E8E8F)
+    
+    convenience init(hex: Int, alpha: CGFloat = 1) {
+        let red = (hex >> 16) & 0xFF
+        let green = (hex >> 8) & 0xFF
+        let blue = hex & 0xFF
+
+        self.init(
+            red: .init(red) / 255,
+            green: .init(green) / 255,
+            blue: .init(blue) / 255,
+            alpha: alpha
+        )
+    }
+}
--- SpaceXApp/SpaceXApp/Extensions/UIView+Ext.swift
+++ SpaceXApp/SpaceXApp/Extensions/UIView+Ext.swift
@@ -5,4 +5,15 @@
 //  Created by Ramazan Abdulaev on 26.07.2022.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
